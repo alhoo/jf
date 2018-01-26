1ce9c2b52c11d6695b039eeccaa587e585260100
Lasse Hyyrynen <hyyrynen@lingsoft.fi>
2018-01-26 20:43:28 +0200

    Add initial code

---
+++ pyq/__main__.py
@@ -0,0 +1,34 @@
+import re
+import sys
+import json
+
+namere = re.compile(r'([{ "\',}]+)')
+
+class Struct:
+  def __init__(self, **entries):
+    for k, v in entries.items():
+      if type(k) in (list, dict):
+        self.__dict__[k] = to_struct(v)
+      else:
+        self.__dict__[k] = v
+    #self.__dict__.update(entries)
+  def __repr__(self):
+    return self__dict
+
+
+def to_struct(v):
+  if type(v) == dict:
+    return Struct(v)
+  if type(v) == list:
+    return [to_struct(a) for a in v]
+  return v
+
+
+def run_query(query, data):
+  query = namere.sub(r'"\1":', query)
+  return json.dumps(eval(query, {"x": to_struct(data)}))
+
+
+if __name__ == '__main__':
+  for d in sys.stdin:
+    print(run_query(sys.argv[1], d))
