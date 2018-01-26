548e01f233cf7b050333679fac0e8a2929557a65
Lasse Hyyrynen <hyyrynen@lingsoft.fi>
2018-01-26 21:06:57 +0200

    Add more tests

--- tests/query_test.py
+++ tests/query_test.py
@@ -8,8 +8,24 @@ import pyq
 class Test_pyq(unittest.TestCase):
 
     def test_simple_query(self):
-        """Load all the test files to be processes"""
+        """Test simple query"""
 
         data = {"a": 1}
         self.assertEqual(pyq.run_query("x.a", data), '1')
+
+    def test_complex_query(self):
+        """Test complex query"""
+
+        data = {"a": 1, 'b': {'c': 632, 'd': [1,2,3,4]}}
+        cmd = '{"id": x.a, "data": x.b.d}'
+        result = '{"data": [1, 2, 3, 4], "id": 1}'
+        self.assertEqual(pyq.run_query(cmd, data, sort_keys=True), result)
+
+    def test_non_json_query(self):
+        """Test complex query"""
+
+        data = {"a": 1, 'b': {'c': 632, 'd': [1,2,3,4]}}
+        cmd = '{id: x.a, data: x.b.d}'
+        result = '{"data": [1, 2, 3, 4], "id": 1}'
+        self.assertEqual(pyq.run_query(cmd, data, sort_keys=True), result)
 
