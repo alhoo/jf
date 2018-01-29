55b70fe48de9030213ccc0bd804ca30afef5c56d
Lasse Hyyrynen <lasse.hyyrynen@utopiaanalytics.com>
2018-01-29 08:37:11 +0200

    Fix colors

--- pyq/__main__.py
+++ pyq/__main__.py
@@ -5,20 +5,13 @@ import argparse
 import logging
 import html
 from pyq import *
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import TerminalFormatter
 #from __init__ import *
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[32m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 def main():
   parser = argparse.ArgumentParser()
@@ -34,19 +27,15 @@ def main():
   if args.debug:
     logger.setLevel(logging.DEBUG)
 
-  colors = {
-            re.compile(r'("[^,:][^"]+")([^:])'): ["bcolors.OKGREEN", ",bcolors.ENDC"],
-            re.compile(r'("[^,:][^"]+")(:)'): ["bcolors.OKBLUE" + "bcolors.BOLD", ":bcolors.ENDC"],
-           }
-
   inq = (json.loads(d) for d in sys.stdin)
+  lexer = get_lexer_by_name("json", stripall=True)
+  formatter = TerminalFormatter()
   for out in run_query(args.query, inq):
     ret = json.dumps(out, sort_keys=args.sort_keys, indent=args.indent, cls=StructEncoder, ensure_ascii=args.ensure_ascii)
     if args.html_unescape:
       ret = html.unescape(ret)
     if args.color:
-      for key, value in colors.items():
-        ret = key.sub(value[0] + r'\1' + value[1] + r"\2", ret)
+      ret = highlight(ret, lexer, formatter).rstrip()
     print(ret)
 
 if __name__ == "__main__":
