673f7359b72f90966816c97a9a95790b5693b6fc
Lasse Hyyrynen <lasse@hyyrynen.net>
2019-02-24 10:47:40 +0200

    add missing unittests

--- jf/process.py
+++ jf/process.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone
 from itertools import islice
 from collections import deque, OrderedDict
 from jf.output import result_cleaner
-from jf.meta import OrderedStruct, to_struct_gen
+from jf.meta import to_struct_gen
 
 logger = logging.getLogger(__name__)
 
@@ -110,23 +110,23 @@ def flatten(*args):
     """
     logger.info("Flattening")
     arr = args[-1]
-    try:
-        for it in map(result_cleaner, arr):
-            yield flatten_item(it)
-    except TypeError as err:
-        logger.error("Got an value error while flattening dict %s", err)
+    for it in map(result_cleaner, arr):
+        yield flatten_item(it)
 
 
 def transpose(*args):
+    """ Transpose input
+    >>> data = [{'a': 1, 'b': 2}, {'a': 2, 'b': 3}]
+    >>> arr = to_struct_gen(data)
+    >>> list(sorted(map(lambda x: list(x.items()), transpose(arr)), key=lambda x: x[0][1]))
+    [[(0, 1), (1, 2)], [(0, 2), (1, 3)]]
+    """
     import pandas as pd
     arr = args[-1]
     data = [x.dict() for x in arr]
-    # data = arr[0]
-    df = pd.DataFrame(data).T
-    df['key'] = df.index
-    df.columns = ['value', 'key']
-    for it in df[['key', 'value']].T.to_dict(into=OrderedDict).values():
-        yield OrderedStruct(it)
+    df = pd.DataFrame(data)
+    for it in df.to_dict(into=OrderedDict).values():
+        yield it
 
 
 def reduce_list(*args):
@@ -145,7 +145,11 @@ def yield_all(fun, arr):
 
 
 def group_by(fun, arr):
-    """Group items by value"""
+    """Group items by value
+    >>> arr = [{'item': '1', 'v': 2},{'item': '2', 'v': 3},{'item': '1', 'v': 3}]
+    >>> list(sorted(map(lambda x: len(x['items']), group_by(lambda x: x['item'], arr))))
+    [1, 2]
+    """
     ret = {}
     for item in arr:
         val = fun(item)
